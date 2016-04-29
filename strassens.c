444cc20081ce079e83f4167d936ddb3652aec039
dmcoley <dmcoley@uw.edu>
2016-04-29 01:13:37 -0700

    initial commit

---
+++ strassens.c
@@ -0,0 +1,17 @@
+/*
+ * Developed by: Derek Coley & Jake Magill
+ * Provides an implementation of Strassen's algorithm
+ * and uses the best threshold value for a hybrid
+ * algorithm of matrix multiplication over integers
+ */
+#include <stdlib.h>
+void strassens(int);
+
+int main(int argc, char** argv) {
+  return EXIT_SUCCESS;
+}
+
+/* TODO: implement */
+void strassens(int n) {
+
+}
