7ff0178c2b72d963ea8eb244ba6f54ab83651c3c
dmcoley <dmcoley@uw.edu>
2016-05-02 12:12:35 -0700

    4x4 works :)

--- strassens.c
+++ strassens.c
@@ -9,7 +9,7 @@
 #include <assert.h>
 
 void checkCorrect(int**, int**, int**, int, int);
-int** strassens(int**, int**, int**, int, int, int);
+int** strassens(int**, int**, int, int, int);
 int** naive(int**, int, int**, int);
 void print(int**, int, int);
 int** createMatrix(int, int);
@@ -20,9 +20,8 @@ int main(int argc, char** argv) {
   int size = 4;
   int** a = createMatrix(size, size);
   int** b = createMatrix(size, size);
-  int** c = createMatrix(size, size);
-  strassens(a, b, c, 0, 0, size);
-  print(c, size, size);
+  print(b, size, size);
+  int** c = strassens(a, b, 0, 0, size);
   checkCorrect(a, b, c, size, size);
   return EXIT_SUCCESS;
 }
@@ -78,11 +77,15 @@ void print(int** grid, int row, int col) {
     }
     printf("\n");
   }
+  printf("\n");
 }
 
 void checkCorrect(int** m1, int** m2, int** test, int m1Size, int m2Size) {
   int** m3 = naive(m1, m1Size, m2, m2Size);
+  printf("expected: \n");
   print(m3, m1Size, m1Size);
+  printf("actual: \n");
+  print(test, m1Size, m1Size);
   for (int i = 0; i < m1Size; i++) {
     for (int j = 0; j < m1Size; j++) {
       assert(test[i][j] == m3[i][j]);
@@ -90,9 +93,9 @@ void checkCorrect(int** m1, int** m2, int** test, int m1Size, int m2Size) {
   }
 }
 
-int** strassens(int** m1, int** m2, int** result, int i, int j, int n) {
-  if (n == 2 || 1) {
-    int** result = createMatrix(2, 2);
+int** strassens(int** m1, int** m2, int i, int j, int n) {
+  if (n == 2) {
+    int** result = createMatrix(n, n);
     int p1 = m1[i][j+1] * (m2[i][j] + m2[i+1][j]);
     int p2 = m1[i+1][j] * (m2[i][j+1] + m2[i+1][j+1]);
     int p3 = (m1[i][j] - m1[i][j+1]) * m2[i][j];
@@ -100,23 +103,108 @@ int** strassens(int** m1, int** m2, int** result, int i, int j, int n) {
     int p5 = (m1[i+1][j+1] - m1[i][j+1]) * (m2[i+1][j] - m2[i+1][j+1]);
     int p6 = (m1[i][j] - m1[i+1][j]) * (m2[i][j+1] - m2[i][j]);
     int p7 = (m1[i+1][j] - m1[i][j+1]) * (m2[i][j] + m2[i+1][j+1]);
-
     result[i][j] = p1 + p3;
     result[i][j+1] = p2 + p3 + p6 - p7;
     result[i+1][j] = p1 + p4 + p5 + p7;
     result[i+1][j+1] = p2 + p4;
     return result;
   } else {
-    // Top left
-    strassens(m1, m2, result, i, j, n/2);
-    // Top right
-    strassens(m1, m2, result, i + n/2, j, n/2);
-    // Bottom left
-    strassens(m1, m2, result, i, j + n/2, n/2);
-    // Bottom right
-    strassens(m1, m2, result, i + n/2, j + n/2, n/2);
-
-    // Merge
+    int** a11 = createMatrix(n/2, n/2);
+    int** a12 = createMatrix(n/2, n/2);
+    int** a21 = createMatrix(n/2, n/2);
+    int** a22 = createMatrix(n/2, n/2);
+    int** b11 = createMatrix(n/2, n/2);
+    int** b12 = createMatrix(n/2, n/2);
+    int** b21 = createMatrix(n/2, n/2);
+    int** b22 = createMatrix(n/2, n/2);
+    int row;
+    int col;
+    row = col = 0;
+    for (int i = 0; i < n/2; i++) {
+      for (int j = 0; j < n/2; j++) {
+        a11[row][col] = m1[i][j];
+        b11[row][col] = m2[i][j];
+        col++;
+      }
+      col = 0;
+      row++;
+    }
+    row = col = 0;
+    for (int i = 0; i < n/2; i++) {
+      for (int j = n/2; j < n; j++) {
+        a12[row][col] = m1[i][j];
+        b12[row][col++] = m2[i][j];
+      }
+      col = 0;
+      row++;
+    }
+    row = col = 0;
+    for (int i = n/2; i < n; i++) {
+      for (int j = 0; j < n/2; j++) {
+        a21[row][col] = m1[i][j];
+        b21[row][col++] = m2[i][j];
+      }
+      col = 0;
+      row++;
+    }
+    row = col = 0;
+    for (int i = n/2; i < n; i++) {
+      for (int j = n/2; j < n; j++) {
+        a22[row][col] = m1[i][j];
+        b22[row][col++] = m2[i][j];
+      }
+      col = 0;
+      row++;
+    }
+    int** p1 = naive(a12, n/2, matrixOp(b11, b21, n/2, 1), n/2);
+    int** p2 = naive(a21, n/2, matrixOp(b12, b22, n/2, 1), n/2);
+    int** p3 = naive(matrixOp(a11, a12, n/2, 0), n/2, b11, n/2);
+    int** p4 = naive(matrixOp(a22, a21, n/2, 0), n/2, b22, n/2);
+    int** p5 = naive(matrixOp(a22, a12, n/2, 0), n/2, matrixOp(b21, b22, n/2, 0), n/2);
+    int** p6 = naive(matrixOp(a11, a21, n/2, 0), n/2, matrixOp(b12, b11, n/2, 0), n/2);
+    int** p7 = naive(matrixOp(a21, a12, n/2, 0), n/2, matrixOp(b11, b22, n/2, 1), n/2);
+    int** c = createMatrix(n, n);
+    int** c11 = matrixOp(p1, p3, n/2, 1);
+    int** c12 = matrixOp(matrixOp(p2, p3, n/2, 1),
+                         matrixOp(p6, p7, n/2, 0),
+                         n/2, 1);
+    int** c21 = matrixOp(matrixOp(p1, p4, n/2, 1),
+                         matrixOp(p5, p7, n/2, 1),
+                         n/2, 1);
+    int** c22 = matrixOp(p2, p4, n/2, 1);
+    row = col = 0;
+    for (int i = 0; i < n/2; i++) {
+      for (int j = 0; j < n/2; j++) {
+        c[i][j] = c11[row][col++];
+      }
+      col = 0;
+      row++;
+    }
+    row = col = 0;
+    for (int i = 0; i < n/2; i++) {
+      for (int j = n/2; j < n; j++) {
+        c[i][j] = c12[row][col++];
+      }
+      col = 0;
+      row++;
+    }
+    row = col = 0;
+    for (int i = n/2; i < n; i++) {
+      for (int j = 0; j < n/2; j++) {
+        c[i][j] = c21[row][col++];
+      }
+      col = 0;
+      row++;
+    }
+    row = col = 0;
+    for (int i = n/2; i < n; i++) {
+      for (int j = n/2; j < n; j++) {
+        c[i][j] = c22[row][col++];
+      }
+      col = 0;
+      row++;
+    }
+    return c;
   }
 }
 
