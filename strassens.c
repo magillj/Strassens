343ce36b4c83a21c2622661fd4683688497c4356
dmcoley <dmcoley@uw.edu>
2016-05-02 14:03:59 -0700

    added comments

--- strassens.c
+++ strassens.c
@@ -8,20 +8,37 @@
 #include <stdio.h>
 #include <assert.h>
 
+/* Asserts that the passed in matrix is the same
+ * as a matrix computed using the naive algorithm */
 void checkCorrect(int**, int**, int**, int, int);
-int** strassens(int**, int**, int, int, int);
+
+/* Uses Strassen's algorithm to multiply together the
+ * two passed in matrices and return a result matrix */
+int** strassens(int**, int**, int);
+
+/* Uses the naive multiplication algorithm to multiply together 
+ * the two passed in matrices and return a result matrix */
 int** naive(int**, int, int**, int);
+
+/* Prints the passed in matrix to stdout */
 void print(int**, int, int);
+
+/* Creates a matrix with values 1, ..., n and returns it */
 int** createMatrix(int, int);
+
+/* Helper function to compute the value for a single
+ * cell in the matrix for the naive algorithm */
 int computeCell(int**, int**, int, int, int);
+
+/* Adds or subtracts the two passed in matrices based
+ * on the flag passed in and returns a result matrix */
 int** matrixOp(int**, int**, int, int);
 
 int main(int argc, char** argv) {
-  int size = 4;
+  int size = 2;
   int** a = createMatrix(size, size);
   int** b = createMatrix(size, size);
-  print(b, size, size);
-  int** c = strassens(a, b, 0, 0, size);
+  int** c = strassens(a, b, size);
   checkCorrect(a, b, c, size, size);
   return EXIT_SUCCESS;
 }
@@ -82,20 +99,25 @@ void print(int** grid, int row, int col) {
 
 void checkCorrect(int** m1, int** m2, int** test, int m1Size, int m2Size) {
   int** m3 = naive(m1, m1Size, m2, m2Size);
-  printf("expected: \n");
-  print(m3, m1Size, m1Size);
-  printf("actual: \n");
-  print(test, m1Size, m1Size);
   for (int i = 0; i < m1Size; i++) {
     for (int j = 0; j < m1Size; j++) {
+      if (test[i][j] != m3[i][j]) {
+        printf("expected: \n");
+        print(m3, m1Size, m1Size);
+        printf("actual: \n");
+        print(test, m1Size, m1Size);
+        printf("failed :( \n");
+      }
       assert(test[i][j] == m3[i][j]);
     }
   }
+  printf("passed :) \n");
 }
 
-int** strassens(int** m1, int** m2, int i, int j, int n) {
-  if (n == 2) {
+int** strassens(int** m1, int** m2, int n) {
+  if (0) {
     int** result = createMatrix(n, n);
+    /*
     int p1 = m1[i][j+1] * (m2[i][j] + m2[i+1][j]);
     int p2 = m1[i+1][j] * (m2[i][j+1] + m2[i+1][j+1]);
     int p3 = (m1[i][j] - m1[i][j+1]) * m2[i][j];
@@ -107,8 +129,10 @@ int** strassens(int** m1, int** m2, int i, int j, int n) {
     result[i][j+1] = p2 + p3 + p6 - p7;
     result[i+1][j] = p1 + p4 + p5 + p7;
     result[i+1][j+1] = p2 + p4;
+    */
     return result;
   } else {
+    /* Split m1 and m2 into four quadrants */
     int** a11 = createMatrix(n/2, n/2);
     int** a12 = createMatrix(n/2, n/2);
     int** a21 = createMatrix(n/2, n/2);
@@ -156,6 +180,8 @@ int** strassens(int** m1, int** m2, int i, int j, int n) {
       col = 0;
       row++;
     }
+
+    /* Use four quadrants and seven multiplications to compute p1, ..., p7 */
     int** p1 = naive(a12, n/2, matrixOp(b11, b21, n/2, 1), n/2);
     int** p2 = naive(a21, n/2, matrixOp(b12, b22, n/2, 1), n/2);
     int** p3 = naive(matrixOp(a11, a12, n/2, 0), n/2, b11, n/2);
@@ -163,6 +189,8 @@ int** strassens(int** m1, int** m2, int i, int j, int n) {
     int** p5 = naive(matrixOp(a22, a12, n/2, 0), n/2, matrixOp(b21, b22, n/2, 0), n/2);
     int** p6 = naive(matrixOp(a11, a21, n/2, 0), n/2, matrixOp(b12, b11, n/2, 0), n/2);
     int** p7 = naive(matrixOp(a21, a12, n/2, 0), n/2, matrixOp(b11, b22, n/2, 1), n/2);
+
+    /* Build result matrix and return */
     int** c = createMatrix(n, n);
     int** c11 = matrixOp(p1, p3, n/2, 1);
     int** c12 = matrixOp(matrixOp(p2, p3, n/2, 1),
